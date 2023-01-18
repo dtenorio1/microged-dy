d3ec0619bff3ee1d73b6bf6d50f4e20c75957b4b
dtenorio1 <122943063+dtenorio1@users.noreply.github.com>
2023-01-17 19:13:10 -0500

    Add files via upload

---
+++ dbformulario.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE `tabla_form` (
+  `id` int(5) NOT NULL,
+  `nombre` varchar(10) NOT NULL,
+  `usuario` varchar(5) NOT NULL,
+  `correo` varchar(5) NOT NULL,
+  `contraseña` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
